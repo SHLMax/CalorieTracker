22223cdef252555bb31f227867dab9b930dcfe91
Max Song <msong@atlassian.com>
2021-11-08 22:37:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/calorietracker/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.calorietracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavigationBarView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnItemSelectedListener(bottomnavFunction);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+    }
+
+    private NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item){
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.food:
+                    fragment = new FoodFragment();
+                    break;
+                case R.id.graph:
+                    fragment = new GraphFragment();
+                    break;
+                case R.id.friend:
+                    fragment = new FriendFragment();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+
+            return true;
+        }
+    };
+}
